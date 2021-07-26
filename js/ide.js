f561f86f631a4d5293dea94e7ce82693c0eadb42
Ravindra barthwal <ravindrabarthwal@users.noreply.github.com>
2021-07-26 21:39:02 +0530

    Change judge0 url

--- js/ide.js
+++ js/ide.js
@@ -1,4 +1,4 @@
-var defaultUrl = localStorageGetItem("api-url") || "https://ce.judge0.com";
+var defaultUrl = localStorageGetItem("api-url") || "http://judge0.centralindia.cloudapp.azure.com:2358";
 var apiUrl = defaultUrl;
 var wait = localStorageGetItem("wait") || false;
 var check_timeout = 300;
